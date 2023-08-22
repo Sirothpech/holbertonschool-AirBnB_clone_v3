0cfdedfc23f1591fb8f2cd39be8375ccb3482d0c
Sirothpech <christophe.ngan@gmail.com>
2023-08-22 14:37:33 +0200

    Fix the slash on the shebang

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,4 +1,4 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 """ Module for the app view """
 from flask import Flask
 import os
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,4 +1,4 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 """ Module for the app view """
 from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
