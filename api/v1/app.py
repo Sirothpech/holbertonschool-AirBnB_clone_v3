f91100ab40bb6261a280cd5648f05003e874d044
Sirothpech <christophe.ngan@gmail.com>
2023-08-22 14:59:27 +0200

    Updated: app.py the method error 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Module for the app view """
-from flask import Flask
+from flask import Flask, jsonify
 import os
 from models import storage
 from api.v1.views import app_views
@@ -9,6 +9,13 @@ app = Flask(__name__)
 
 
 app.register_blueprint(app_views)
+
+
+# app name
+@app.errorhandler(404)
+def not_found(e):
+    """ 404 error handler """
+    return jsonify({'error': 'Not found'}), 404
 
 
 @app.teardown_appcontext
